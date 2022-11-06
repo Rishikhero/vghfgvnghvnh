ecf58b68448ed34b1e7cc4e2cda93b69815e57c3
Rishikhero <97271798+Rishikhero@users.noreply.github.com>
2022-11-06 11:41:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -20,3 +20,26 @@ function take_snapshot(){
 console.log('ml5.version', ml5.version);
 
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/0F3KKtEEM/model.json', modelLoaded );
+
+function modelLoaded(){
+    console.log('model Loaded');
+}
+
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult)
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+
+    }
+    else{
+        console.log(results);
+        document.getElementById('result_object_name').innerHTML = results[0].label;
+        document.getElementById('result_object_accuracy').innerHTML = results[0].confidence.toFixed(3);
+    }
+}
+
+
